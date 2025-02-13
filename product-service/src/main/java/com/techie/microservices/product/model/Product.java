0ed7f10356c795966b28f382f164025dbe30e71c
naffalentera <naffalenteranisa@gmail.com>
2025-02-13 19:37:35 +0700

    part 2:initialize database

---
+++ product-service/src/main/java/com/techie/microservices/product/model/Product.java
@@ -0,0 +1,24 @@
+package com.techie.microservices.product.model;
+
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.annotation.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Document(value = "product")
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Data
+public class Product {
+    @Id
+    private String Id;
+    private String name;
+    private String description;
+    private BigDecimal price;
+}
