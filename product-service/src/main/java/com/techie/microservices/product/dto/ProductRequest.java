d39932310d53b53c547f4974e1138092bd60ade8
naffalentera <naffalenteranisa@gmail.com>
2025-03-05 23:55:10 +0700

    part 9 and 10 done

--- product-service/src/main/java/com/techie/microservices/product/dto/ProductRequest.java
+++ product-service/src/main/java/com/techie/microservices/product/dto/ProductRequest.java
@@ -2,6 +2,5 @@ package com.techie.microservices.product.dto;
 
 import java.math.BigDecimal;
 
-public record ProductRequest(String Id, String name, String description, BigDecimal price) {
-
-}
+public record ProductRequest(String Id, String name, String description,
+                             String skuCode, BigDecimal price) {}
